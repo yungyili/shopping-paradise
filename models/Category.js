eabe0262e39b2dce2082e19ca68b9fbeeee218e9
yyli <yyli>
2018-06-02 15:55:38 +0800

    redesign category as nested set

--- models/Category.js
+++ models/Category.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const NestedSetPlugin = require('mongoose-nested-set');
 
 const CategorySchema = new Schema({
-  path: { type: String, unique: true }
+  title: { type: String, unique: true }
 });
+
+// Include plugin
+CategorySchema.plugin(NestedSetPlugin);
 
 mongoose.model('categories', CategorySchema);
--- routes/categoriesRoutes.js
+++ routes/categoriesRoutes.js
@@ -5,30 +5,99 @@ module.exports = (app) => {
   app.post("/api/category",
     async (req, res) => {
       console.log("post /category:", req.body);
+      var parentId = undefined;
 
-      const category = await Category.findOne({email: req.body.path}).exec();
+      if (!req.body || !req.body.title) {
+        res.sendStatus(400);
+        return;
+      }
+
+      if(req.body.parentId){
+        parentId = req.body.parentId;
+      }
+      const {title} = req.body;
+
+      const category =
+        await Category.findOne({
+          title: title,
+          parentId: parentId
+        }).exec();
 
       if (category) {
         res.sendStatus(406);
         return;
       }
 
-      const {path} = req.body;
-
-      const newCategory = await new Category({
-        path: path
+      await new Category({
+        title: title,
+        parentId: parentId
       }).save();
-      console.log("post /category: newCategory=", newCategory);
 
+      const root = await Category.findOne({parentId:undefined}).exec();
+
+      if (!root){
+        res.sendStatus(400);
+        return;
+      }
+
+      await Category.rebuildTree(root, 1);
       res.json(newCategory);
     });
 
-  app.get("/api/category/:path", /*auth.authenticate(),*/
-    async (req, res) => {
-      console.log("get /category:", req.params.path);
-      const path = `/${req.params.path}`;
-      const category = await Category.findOne({path: path}).exec();
+  app.get("/api/category", /*auth.authenticate(),*/
+      async (req, res) => {
+        console.log("get /category root");
+        const category = await Category.findOne({parentId: undefined}).exec();
 
-      res.json(category);
+        if(!category){
+          res.sendStatus(400);
+          return;
+        }
+
+        await category.selfAndDescendants(function(err, categories){
+          if (err){
+            res.sendStatus(400);
+          }
+          res.json(categories);
+        });
+
+      });
+
+  app.get("/api/category/:id", /*auth.authenticate(),*/
+    async (req, res) => {
+      console.log("get /category:", req.params.id);
+      const category = await Category.findOne({_id: req.params.id}).exec();
+
+      if(!category){
+        res.sendStatus(400);
+        return;
+      }
+
+      await category.selfAndDescendants(function(err, categories){
+        if (err){
+          res.sendStatus(400);
+        }
+        res.json(categories);
+      });
+
     });
+
+    app.get("/api/category/path/:id", /*auth.authenticate(),*/
+      async (req, res) => {
+        console.log("get /category:", req.params.id);
+        const category = await Category.findOne({_id: req.params.id}).exec();
+
+        if(!category){
+          res.sendStatus(400);
+          return;
+        }
+
+        await category.selfAndAncestors(function(err, categories){
+          if (err){
+            res.sendStatus(400);
+          }
+          res.json(categories);
+        });
+
+      });
 }
